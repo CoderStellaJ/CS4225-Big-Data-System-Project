e42f776bce7ca1541b744bc2dfa9a4b022f71303
WangJieee <>
2020-04-05 23:33:55 +0800

    update worker code

--- worker.py
+++ worker.py
@@ -39,19 +39,19 @@ class Worker:
             zoom_range=0.2,
             horizontal_flip=True)
         self.train_generator = train_datagen.flow_from_directory(train_data_dir, target_size=(img_width, img_height),
-                                                            batch_size=batch_size, class_mode='categorical')
+                                                                 batch_size=batch_size, class_mode='categorical')
         self.val_generator = train_datagen.flow_from_directory(val_data_dir, target_size=(img_width, img_height),
-                                                            batch_size=batch_size, class_mode='categorical')
+                                                               batch_size=batch_size, class_mode='categorical')
         self.base = applications.ResNet50(include_top=False, weights='imagenet', input_shape=(img_width, img_height, 3))
-        self.keras_model = self.get_model(base)
+        self.keras_model = self.get_model(self.base)
         self.weight_dict = {}
 
     def get_model(self, base):
-        for layer in base:
-            layer.trainable = True
+        for layer in base.layers:
+            layer.trainable = False
         opt = keras.optimizers.Adam(lr=lr)
         reg = keras.regularizers.l1_l2(l1=l1, l2=l2)
-        x = Flatten()(base)
+        x = Flatten()(base.output)
         x = Dense(nn1, activation='relu', kernel_regularizer=reg)(x)
         x = BatchNormalization()(x)
         x = Dense(nn2, activation='relu', kernel_regularizer=reg)(x)
@@ -59,7 +59,7 @@ class Worker:
         x = Dense(nn3, activation='relu', kernel_regularizer=reg)(x)
         x = BatchNormalization()(x)
         x = Dense(NB_CLASSES, activation='softmax')(x)
-        model = Model(input=input, output=x)
+        model = Model(input=base.input, output=x)
 
         model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
@@ -70,16 +70,21 @@ class Worker:
             for layer_i in range(len(self.keras_model.layers)):
                 self.keras_model.layers[layer_i].set_weights(new_weight['w_' + str(layer_i)])
 
-        history = model.fit_generator(train_generator, steps_per_epoch=2000,
-                                      epochs=1, validation_data=validation_generator)
+        history = self.keras_model.fit_generator(self.train_generator,
+                                                 epochs=1, validation_data=self.val_generator)
 
         # save to weight dict
         for layer_i in range(len(self.keras_model.layers)):
             w = self.keras_model.layers[layer_i].get_weights()
-            print('Layer %s has weights of shape %s' % (
-                layer_i, np.shape(w)))
+            # print('Layer %s has weights of shape %s' % (
+            #   layer_i, np.shape(w)))
 
             # create array to hold weights and biases
             self.weight_dict['w_' + str(layer_i)] = w
 
         return history.history['loss'], history.history['val_loss'], self.weight_dict
+
+    def train_all(self):
+        history = self.keras_model.fit_generator(self.train_generator,
+                                                 epochs=50, validation_data=self.val_generator)
+        return self.keras_model
