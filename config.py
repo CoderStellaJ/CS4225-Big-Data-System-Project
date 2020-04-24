9bd6190e56a64ce71ddca8a9cfccdf9e553afd83
CoderStellaJ <shuyuanj12@gmail.com>
2020-04-24 17:47:26 +0800

    adds hash for partition and set update epoch

--- config.py
+++
@@ -1,5 +0,0 @@
-# when you import: from config import *
-# call e.g. shuffle in your code
-shuffle_data = True
-sync = True
-custom_hash = True   # for partition experiment, please set to False
--- main_spark.py
+++ main_spark.py
@@ -16,28 +16,48 @@ from sklearn.utils import shuffle
 from matplotlib import pyplot as plt
 from PIL import Image
 import customElephas
-from config import *
-
+################################### PARAMETER ########################################
 # Data directory
 DATA_DIR = '/path/to/data'
 
 # SPARK parameters
-NUM_PARTITION = 5
+NUM_PARTITION = 8
+TOTAL_EPOCH = 20
+UPDATE_EPOCH = 1
 
 img_width, img_height = 224,224
 
-# spark
+shuffle_data = True
+sync = True
+custom_hash = True
+
+################################ spark ##################################################
 conf = SparkConf().setAppName('Elephas_App').setMaster('local')
 sc = SparkContext(conf=conf)
 
+################################ data and partition ##############################################
 x_train = np.load(os.path.join(DATA_DIR, 'x_train.npy'))
 y_train = np.load(os.path.join(DATA_DIR, 'y_train.npy'))
+
+x_test = np.load(os.path.join(DATA_DIR, 'x_test.npy'))
+y_test = np.load(os.path.join(DATA_DIR, 'y_test.npy'))
 
 # Shuffle the order?
 if shuffle_data == True:
     x_train, y_train = shuffle(x_train, y_train)
 
 # different number of partitions and different partition distribution
+def data_partitioner(partition_key):
+    index = np.where(partition_key==1)
+    print("hash index:", index[0][0])
+    return index[0][0]
+
+def label_hash(label):
+    print("label is:", label)
+    hash_value = hash(label % NUM_PARTITION)
+    print("hash value:", hash_value)
+    return hash_value
+
 def to_simple_rdd(sc, features, labels):
     """Convert numpy arrays of features and labels into
     an RDD of pairs.
@@ -49,28 +69,21 @@ def to_simple_rdd(sc, features, labels):
     """
     pairs = [(x, y) for x, y in zip(features, labels)]
     if custom_hash == True:
-        rdd = sc.parallelize(pairs, NUM_PARTITION).cache()
+        rdd = sc.parallelize(pairs).map(lambda pair: (data_partitioner(pair[1]), pair)).partitionBy(NUM_PARTITION,
+                                                                                                    label_hash)
+        rdd = rdd.map(lambda composite_pair: composite_pair[1]).cache()
     else:
         rdd = sc.parallelize(pairs, NUM_PARTITION).cache()
     return rdd
 
-# spark
-conf = SparkConf().setAppName('Elephas_App').setMaster('local')
-sc = SparkContext(conf=conf)
-
 # partition input data
 rdd = to_simple_rdd(sc, x_train, y_train)
-print("Number of partitions: {}".format(rdd.getNumPartitions()))
-print("Partitioner: {}".format(rdd.partitioner))
-print("Partitions structure: {}".format(rdd.glom().collect()))
-
 
 print("Number of partitions: {}".format(rdd.getNumPartitions()))
 print("Partitioner: {}".format(rdd.partitioner))
 print("Partitions structure: {}".format(rdd.glom().collect()))
 
-# create keras model
-# VGG
+################################### keras model ##############################################
 base_model = applications.VGG16(weights='imagenet',include_top= False,input_shape=(224,224,3))
 
 top_model = Sequential()
@@ -90,14 +103,16 @@ for layer in model.layers[:16]:
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.Adam(lr=0.0001, beta_1=0.9,beta_2=0.999,epsilon=1e-8, decay=0.0),metrics=['accuracy'])
 
-# Create SPARK model
+################################### Spark Model ###############################################
 # spark_model = SparkModel(model, frequency='epoch', mode='asynchronous')
 if sync == True:
-    spark_model = customElephas.CustomSparkModel(model, frequency='epoch', mode='synchronous')
+    for i in range(TOTAL_EPOCH/UPDATE_EPOCH):
+      spark_model = customElephas.CustomSparkModel(model, frequency='epoch', mode='synchronous')
+      spark_model.get_config()
+      spark_model.fit(rdd, epochs=UPDATE_EPOCH, batch_size=128, verbose=1)
+      model = spark_model.master_network
+      score = spark_model.master_network.evaluate(x_test, y_test)
+      print('Test accuracy:', score[1])
 else:
     spark_model = customElephas.CustomSparkModel(model, frequency='epoch', mode='asynchronous')
-
-history = spark_model.fit(rdd, epochs=20, batch_size=32, verbose=1, validation_split=0.1)
-
-
-
+    spark_model.fit(rdd, epochs=20, batch_size=32, verbose=1, validation_split=0.1)
